f3c0869b8f5c9b4ff275c6cfd7e406be21debb0d
Kawsar-Ahmed-Ridoy <kawsarahmedridoy1@gmail.com>
2022-10-27 05:14:55 +0600

    create some component, and route file inside (home, category, courses) added

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p><small>ami taratari aaitasi</small></p>
+        </div>
+    );
+};
+
+export default Footer;
