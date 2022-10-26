bb299519634ecb7e9560934ca693f88ac4a6dfc5
Kawsar-Ahmed-Ridoy <kawsarahmedridoy1@gmail.com>
2022-10-27 04:10:49 +0600

    create new react app

---
+++ components/Home.js
@@ -0,0 +1,12 @@
+import { Button } from 'bootstrap';
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <Button>happy</Button>
+        </div>
+    );
+};
+
+export default Home;
