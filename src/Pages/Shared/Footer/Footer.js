684c399a2735b02264bfba05a2ce356edf7c8238
Kawsar-Ahmed-Ridoy <kawsarahmedridoy1@gmail.com>
2022-10-28 16:30:29 +0600

    footer updated

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -2,17 +2,19 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer style={{
+        <div className='mt-5'>
+          <footer style={{
         background: "white",
         position: "fixed",
         bottom: "0",
         margin: "auto",
         width: "100%"
       }}>
-      <p className="text-center align-content-end text-secondary py-4 border m-0">
+      <p className="text-center align-content-end text-secondary py-3 border m-0">
         Copyright 2022 Our Web School
       </p>
     </footer>
+        </div>
     );
 };
 
